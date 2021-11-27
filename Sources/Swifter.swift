9d058b1eec7dff006634ed1cbc8d009972493b02
Kemal Şen <kemal.sen>
2021-11-27 16:35:16 +0300

    Twitter v2

--- Sources/Swifter.swift
+++ Sources/Swifter.swift
@@ -49,11 +49,11 @@ public enum TwitterURL {
     
     var url: URL {
         switch self {
-        case .api:          return URL(string: "https://api.twitter.com/1.1/")!
-        case .upload:       return URL(string: "https://upload.twitter.com/1.1/")!
-        case .stream:       return URL(string: "https://stream.twitter.com/1.1/")!
-        case .userStream:   return URL(string: "https://userstream.twitter.com/1.1/")!
-        case .siteStream:   return URL(string: "https://sitestream.twitter.com/1.1/")!
+        case .api:          return URL(string: "https://api.twitter.com/2/")!
+        case .upload:       return URL(string: "https://upload.twitter.com/2/")!
+        case .stream:       return URL(string: "https://stream.twitter.com/2/")!
+        case .userStream:   return URL(string: "https://userstream.twitter.com/2/")!
+        case .siteStream:   return URL(string: "https://sitestream.twitter.com/2/")!
         case .oauth:        return URL(string: "https://api.twitter.com/")!
         case .publish:		return URL(string: "https://publish.twitter.com/")!
         }
